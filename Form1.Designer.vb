bb3293c6d4fae2a40beaba3626c2e29a38df6bc5
Jesse Donat <donatj@gmail.com>
2010-11-24 13:57:30 -0600

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.FileBox = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextErrors = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'FileBox
+        '
+        Me.FileBox.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.FileBox.Location = New System.Drawing.Point(12, 12)
+        Me.FileBox.Multiline = True
+        Me.FileBox.Name = "FileBox"
+        Me.FileBox.Size = New System.Drawing.Size(264, 479)
+        Me.FileBox.TabIndex = 0
+        Me.FileBox.Text = resources.GetString("FileBox.Text")
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(577, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Scan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextErrors
+        '
+        Me.TextErrors.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextErrors.Location = New System.Drawing.Point(282, 67)
+        Me.TextErrors.Multiline = True
+        Me.TextErrors.Name = "TextErrors"
+        Me.TextErrors.Size = New System.Drawing.Size(370, 424)
+        Me.TextErrors.TabIndex = 2
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(282, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(290, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Location = New System.Drawing.Point(578, 11)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(74, 21)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Browse"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(664, 503)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextErrors)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.FileBox)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "FileExist-O-Tron"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents FileBox As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextErrors As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,92 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim skipped As New List(Of String)
+        Dim notfound As New List(Of String)
+        Dim issues As New List(Of String)
+
+        Dim fsentries As String() = IO.Directory.GetFiles(TextBox1.Text, "*.*", System.IO.SearchOption.AllDirectories)
+
+        For Each line As String In FileBox.Lines
+            line = line.Trim
+            If line.Length > 0 Then
+
+                If line.Contains(".") And Not line.Contains("/") And Not line.Contains("\") Then
+                    Dim local_line As String = line
+
+                    Dim listed As Boolean = fsentries.Where(Function(a) a.EndsWith("\" & local_line)).Count > 0
+                    Dim exists As Boolean = System.IO.File.Exists(TextBox1.Text & line)
+
+                    If Not listed And Not exists Then
+                        notfound.Add(line)
+                    ElseIf Not listed Then
+                        issues.Add(line)
+                    End If
+
+                Else
+                    skipped.Add(line)
+                End If
+            End If
+        Next
+
+        TextErrors.Text = ""
+        If notfound.Count > 0 Then
+            TextErrors.Text &= "---Not Found---" & vbCrLf
+            TextErrors.Text &= Join(notfound.ToArray, vbCrLf)
+            TextErrors.Text &= vbCrLf & vbCrLf
+        End If
+
+        If skipped.Count > 0 Then
+            TextErrors.Text &= "---Skipped---" & vbCrLf
+            TextErrors.Text &= Join(skipped.ToArray, vbCrLf)
+            TextErrors.Text &= vbCrLf & vbCrLf
+        End If
+
+        If issues.Count > 0 Then
+            TextErrors.Text &= "---Capitilization Mismatch---" & vbCrLf
+            TextErrors.Text &= Join(issues.ToArray, vbCrLf)
+            TextErrors.Text &= vbCrLf & vbCrLf
+        End If
+
+        If TextErrors.Text.Trim.Length = 0 Then
+            TextErrors.Text = "No Issues, Woohoo!!!"
+        End If
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = My.Settings.LastPath.Trim
+        If TextBox1.Text = "" Or TextBox1.Text = "\" Then
+            TextBox1.Text = My.Computer.FileSystem.SpecialDirectories.Desktop
+        End If
+        FileBox.Text = My.Settings.LastFiles
+    End Sub
+
+    Private Sub TextErrors_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextErrors.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        FolderBrowserDialog1.ShowDialog()
+        TextBox1.Text = FolderBrowserDialog1.SelectedPath
+    End Sub
+
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        If Not TextBox1.Text.EndsWith("\") Then
+            TextBox1.Text &= "\"
+        End If
+
+        If TextBox1.Text = "\" Then
+            TextBox1.Text = My.Computer.FileSystem.SpecialDirectories.Desktop
+        End If
+
+        My.Settings.LastPath = TextBox1.Text
+    End Sub
+
+    Private Sub FileBox_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FileBox.TextChanged
+        My.Settings.LastFiles = FileBox.Text
+    End Sub
+End Class
